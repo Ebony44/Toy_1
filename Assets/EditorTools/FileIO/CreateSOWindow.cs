14521a1583e7b96a8bfdbba08aff5b615b67b087
Ebony04 <ebonytroll@gmail.com>
2022-08-08 12:54:52 +0900

    Editor tools commit - Readonly attribute - Scriptable object script

---
+++ Assets/EditorTools/FileIO/CreateSOWindow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+public class CreateSOWindow : MonoBehaviour
+{
+    public void GetActiveProfileFolder()
+    {
+        var path = string.Empty;
+        var _tryGetActiveFolderPath = typeof(ProjectWindowUtil).GetMethod("TryGetActiveFolderPath", BindingFlags.Static | BindingFlags.NonPublic);
+
+        object[] args = new object[] { null };
+        bool found = (bool)_tryGetActiveFolderPath.Invoke(null, args);
+        path = (string)args[0];
+
+        Debug.Log("current path is " + path);
+
+
+    }
+}
