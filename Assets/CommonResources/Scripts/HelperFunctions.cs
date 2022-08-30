266fd5a7bfb039a3e235fb739e1cf519382e781d
Ebony04 <ebonytroll@gmail.com>
2022-08-30 18:35:20 +0900

    Material creating editor window tool commit

--- Assets/CommonResources/Scripts/HelperFunctions.cs
+++ Assets/CommonResources/Scripts/HelperFunctions.cs
@@ -1,15 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public static class HelperFunctions
 {
 
+    #region isometric control and view
     private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
 
     public static Vector3 ToIso(this Vector3 input)
     {
         return _isoMatrix.MultiplyPoint3x4(input);
     }
+    #endregion
+
+    #region file IO
+    public static void CreateDirectory(string folderPath)
+    {
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+    }
+
+    public static string[] GetFilesfullPath(string folderPath)
+    {
+        string[] fileNames;
+        fileNames = Directory.GetFiles(folderPath);
+
+        
+        return fileNames;
+    }
+
+    public static string[] FindMatchAfterCertainString(string rawString, string certainStringPat)
+    {
+        
+        // var tempRegex = new Regex(@"(?<=[^{folderPath}\/])(\w+)");
+        //var str = "=IIF(IsNothing(folderPath!Metadata1.Value),\"N / A\"," + rawString + "!Metadata4.Value)" + "TestMatFolder" + "!Metadata6.Value"
+        //    + "er!Metadata77.Value";
+        // var tempRegex = new Regex($@"(?<=[{folderPath}]!)(\w+)");
+        // var tempPattern = @"(?<=" + rawString + "!)(\\w+)";
+        var tempPattern = @"(?<=" + certainStringPat + ")([0-9]*)";
+        
+        
+
+        var tempRegex = new Regex(tempPattern);
+        var matches = tempRegex.Matches(rawString);
+        foreach (var item in matches)
+        {
+            Debug.Log("matches are " + item);
+        }
+        // MatchCollection
+        #region
+        //var testString = "Mat_1234" + ", Mat_3456";
+        //var tempPattern2 = @"(?<=" + "Mat_" + ")([0-9]*)";
+        //var tempRegex2 = new Regex(tempPattern2);
+        //var matches2 = tempRegex2.Matches(testString);
+        //foreach (var item in matches2)
+        //{
+        //    Debug.Log("matches are " + item.ToString());
+        //}
+        #endregion
+
+        string[] result = new string[matches.Count];
+        for (int i = 0; i < result.Length; i++)
+        {
+            var temp = matches[i];
+            result[i] = matches[i].Value;
+        }
+
+
+        return result;
+
+    }
+
+    public static string[] FindMatchAfterCertainString(string[] rawStrings, string certainStringPat)
+    {
+        string result = string.Empty;
+        StringBuilder sb = new StringBuilder();
+        foreach (var item in rawStrings)
+        {
+            if(string.IsNullOrEmpty(item))
+            {
+                Debug.Log("[FindMatchAfterCertainString], item is null or empty");
+                continue;
+            }
+            sb.Append(item);
+            sb.Append(", ");
+        }
+        result = sb.ToString();
+        return FindMatchAfterCertainString(result, certainStringPat);
+    }
+
+    public static int ParseAndReturnNumber(bool bHighestReturn, string[] numberStrings)
+    {
+        var result = 0;
+        var tempValue = 0;
+        foreach (var item in numberStrings)
+        {
+            int.TryParse(item, out tempValue);
+            if(bHighestReturn == false)
+            {
+                if(result >= tempValue)
+                {
+                    result = tempValue;
+                }
+            }
+            else
+            {
+                if (result <= tempValue)
+                {
+                    result = tempValue;
+                }
+            }
+        }
+        return result;
+    }
+
+    #endregion
+
+    
+
+
+
+
 
 }
--- Assets/EditorTools/FileIO/MaterialCreateWindow.cs
+++ Assets/EditorTools/FileIO/MaterialCreateWindow.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public class MaterialCreateWindow : EditorWindow
 {
@@ -11,6 +13,9 @@ public class MaterialCreateWindow : EditorWindow
 
     public Object assetSrcFolderPath;
     public Object assetDstPath;
+
+    public bool bIsFrom3dTexture;
+    public bool bIsFromPoliigon;
 
     [MenuItem("Tools/MaterialCreator")]
     public static void Open()
@@ -23,6 +28,16 @@ public class MaterialCreateWindow : EditorWindow
 
     private void OnGUI()
     {
+        GUILayout.Space(20f);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Poliigon", GUILayout.Width(100f));
+        EditorGUILayout.LabelField("3DTexture", GUILayout.Width(100f));
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(5f);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.Toggle(bIsFromPoliigon, GUILayout.Width(100f));
+        EditorGUILayout.Toggle(bIsFrom3dTexture, GUILayout.Width(100f));
+        EditorGUILayout.EndHorizontal();
         GUILayout.Space(20f);
 
         // EditorGUILayout.ObjectField()
@@ -115,17 +130,58 @@ public class MaterialCreateWindow : EditorWindow
         return path;
     }
 
-    static void CreateMaterial(string path)
+    static void CreateMaterial(string folderPath)
     {
-        // Create a simple material asset
+        // 0. check directory path and folder
+        HelperFunctions.CreateDirectory(folderPath);
 
+        // 1. check any file name and make it count
+        var tempFiles = Directory.GetFiles(folderPath);
+
+        string[] tempFileNames = new string[tempFiles.Length];
+        var tempIndex = 0;
+        foreach (var file in tempFiles)
+        {
+            if (file.Contains(".meta"))
+            {
+                continue;
+            }
+            Debug.Log("file name is " + Path.GetFileName(file));
+            tempFileNames[tempIndex] = Path.GetFileName(file);
+            ++tempIndex;
+        }
+
+        var recentFileName = string.Empty;
+
+        var tempMatches = HelperFunctions.FindMatchAfterCertainString(tempFileNames, "Mat_");
+        var tempNumber = HelperFunctions.ParseAndReturnNumber(true, tempMatches);
+        Debug.Log("highest number " + tempNumber);
+        foreach (var item in tempMatches)
+        {
+            Debug.Log("Match is " + item);
+        }
+
+
+
+
+        //// 2. Create a simple material asset
+        
         Material material = new Material(Shader.Find("Standard"));
-        var tempNumber = 0;
-        AssetDatabase.CreateAsset(material, path + "/Mat_" + tempNumber + ".mat");
-        // path is Assets/TempAssetSources/Materials/TestMatFolder
+        ++tempNumber;
+        AssetDatabase.CreateAsset(material, folderPath + "/Mat_" + tempNumber + ".mat");
 
-        // Print the path of the created asset
+        //// path is Assets/TempAssetSources/Materials/TestMatFolder
+
+        //// Print the path of the created asset
+
         Debug.Log(AssetDatabase.GetAssetPath(material));
+
+        // 3. set that asset's property...
+
+        // material.SetTexture("Albedo",)
+
+
+
     }
 
     private void DisplayPath(Object folderReference)
@@ -136,6 +192,11 @@ public class MaterialCreateWindow : EditorWindow
 
         var path = AssetDatabase.GetAssetPath(folderReference);
         Debug.Log("path is " + path);
+    }
+
+    private void GetFileFromPaths()
+    {
+
     }
 
 }
