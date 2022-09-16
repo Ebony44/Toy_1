0e7e061b14c4ff07a2dd544401ebe2571943b0c8
Sandalwood <ebonytroll@gmail.com>
2022-09-16 18:21:24 +0900

    file downloading code snippet commit

---
+++ Assets/EditorTools/FileIO/FileDownloader.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+// using System.Net;
+using System.Net.Http;
+using UnityEditor;
+using UnityEngine;
+
+
+public class FileDownloader : EditorWindow
+{
+
+    private void OnGUI()
+    {
+        
+    }
+
+    public void OnDownloadButton()
+    {
+        using (var client = new HttpClient())
+        {
+            using (var s = client.GetStreamAsync("https://via.placeholder.com/150"))
+            {
+                using (var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate))
+                {
+                    s.Result.CopyTo(fs);
+                }
+            }
+        }
+
+        //using var client = new HttpClient();
+        //using var s = await client.GetStreamAsync("https://via.placeholder.com/150");
+        //using var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate);
+        //await s.CopyToAsync(fs);
+    }
+    public async void OnDownloadButtonAsync()
+    {
+        using var client = new HttpClient();
+        using var s = await client.GetStreamAsync("https://via.placeholder.com/150");
+        using var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate);
+        await s.CopyToAsync(fs);
+    }
+
+
+
+}
