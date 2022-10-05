401228d26609b58f251711cbbaed12abdb71513b
Sandalwood <ebonytroll@gmail.com>
2022-10-05 17:56:10 +0900

    Some input action and navmesh fiddling

--- Assets/TempAssetSources/Scripts/TestPurpose/NavMesh/TestNavMeshController.cs
+++ Assets/TempAssetSources/Scripts/TestPurpose/NavMesh/TestNavMeshController.cs
@@ -11,9 +11,42 @@ public class TestNavMeshController : MonoBehaviour
 
     private RaycastHit[] Hits = new RaycastHit[1];
 
+    [SerializeField] private InputActionAsset inputMap;
+
+    
+
+    InputAction click;
+    InputAction pos;
+
     private void Awake()
     {
         Agent = GetComponent<ObstacleAgent>();
+        // playerInput = new PlayerInput();
+        click = inputMap.FindAction("Fire");
+        pos = inputMap.FindAction("MousePosition");
+
+        click.performed += Process;
+
+    }
+    private void OnEnable()
+    {
+        inputMap.Enable();
+        
+    }
+
+    public void Process(InputAction.CallbackContext value)
+    {
+        Debug.Log($"Input action: {pos.ReadValue<Vector2>()}");
+
+        Vector3 tempVector = pos.ReadValue<Vector2>();
+        tempVector.z = Camera.main.farClipPlane * 0.5f;
+        Ray ray = Camera.ScreenPointToRay(tempVector);
+        
+        if (Physics.RaycastNonAlloc(ray, Hits) > 0)
+        {
+            Agent.SetDestination(Hits[0].point);
+        }
+
     }
 
     //private void Update()
@@ -33,13 +66,29 @@ public class TestNavMeshController : MonoBehaviour
 
     public void OnMouseClick(InputAction.CallbackContext value)
     {
-        var tempVector = value.ReadValue<Vector2>();
-        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+        var temp = value;
+        Debug.Log("value is " + value);
 
-        if (Physics.RaycastNonAlloc(ray, Hits) > 0)
-        {
-            Agent.SetDestination(Hits[0].point);
-        }
+        // var tempVector = mouse.valueType
+
+        // var mousePosition = PlayerInput.ControlsChangedEvent
+        // var mousePositionZ = Camera.farClipPlane * .5f;
+        // var mouseViewportPosition = _camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, mousePositionZ));
+
+
+        // var tempVector = value.ReadValue<Vector2>();
+
+        // Vector3 tempVector = Mouse.current.position.ReadValue();
+
+
+
+        // tempVector.z = Camera.main.farClipPlane * 0.5f;
+        // Ray ray = Camera.ScreenPointToRay(tempVector);
+        // 
+        // if (Physics.RaycastNonAlloc(ray, Hits) > 0)
+        // {
+        //     Agent.SetDestination(Hits[0].point);
+        // }
 
     }
 }
