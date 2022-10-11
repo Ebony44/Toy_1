11fec296951d09191ab36484171d53968f01455b
Sandalwood <ebonytroll@gmail.com>
2022-10-11 18:32:39 +0900

    something with visualization and related editor tool commit

---
+++ Assets/EditorTools/Visualisation/FieldOfViewEditor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor (typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    
+    private void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward,360f,fow.viewRadius);
+    }
+}
