7fddbd47cb4a495c438d2a77cdc168352b6602fe
Sandalwood <ebonytroll@gmail.com>
2023-07-26 18:30:24 +0900

    line equation practice

--- Assets/LessonLearned/Maths/Scripts/LineVisualForMathPractice.cs
+++ Assets/LessonLearned/Maths/Scripts/LineVisualForMathPractice.cs
@@ -67,9 +67,10 @@ public class LineVisualForMathPractice : MonoBehaviour
         Vector2 endPoint = new Vector2(secondX, secondY);
 
         SetupPos(firstLineStartPointObj, startPoint);
-        var firstMidPointX = (startPoint.x + endPoint.x) / 2f;
-        var firstMidPointY = (startPoint.y + endPoint.y) / 2f;
-        var firstMidPoint = new Vector2(firstMidPointX, firstMidPointY);
+        // var firstMidPointX = (startPoint.x + endPoint.x) / 2f;
+        // var firstMidPointY = (startPoint.y + endPoint.y) / 2f;
+        // var firstMidPoint = new Vector2(firstMidPointX, firstMidPointY);
+        var firstMidPoint = MathHelperFunctions.GetMidPointOfLine(startPoint, endPoint);
 
         SetupPos(firstLineMidPointObj, firstMidPoint);
 
@@ -78,35 +79,94 @@ public class LineVisualForMathPractice : MonoBehaviour
 
         var tempVectors = MathHelperFunctions.GetPerpendicularLine(startPoint, endPoint, secondLineLength, modX, modY);
 
-        SetupPos(secondLineStartPointObj, tempVectors.Item1);
-        var secondMidPointX = (tempVectors.Item1.x + tempVectors.Item2.x) / 2f;
-        var secondMidPointY = (tempVectors.Item1.y + tempVectors.Item2.y) / 2f;
-        var secondMidPoint = new Vector2(secondMidPointX, secondMidPointY);
+        var secondLineStartPoint = tempVectors.Item1;
+        var secondLineEndPoint = tempVectors.Item2;
+
+        SetupPos(secondLineStartPointObj, secondLineStartPoint);
+        // var secondMidPointX = (tempVectors.Item1.x + tempVectors.Item2.x) / 2f;
+        // var secondMidPointY = (tempVectors.Item1.y + tempVectors.Item2.y) / 2f;
+        // var secondMidPoint = new Vector2(secondMidPointX, secondMidPointY);
+        var secondMidPoint = MathHelperFunctions.GetMidPointOfLine(secondLineStartPoint, secondLineEndPoint);
 
         SetupPos(secondLineMidPointObj, secondMidPoint);
 
-        SetupPos(secondLineEndPointObj, tempVectors.Item2);
+        SetupPos(secondLineEndPointObj, secondLineEndPoint);
 
 
-        firstLine.positionCount = 2;
-        secondLine.positionCount = 2;
+        SetupLine(0, startPoint, endPoint);
+        SetupLine(1, tempVectors.Item1, tempVectors.Item2);
+        //firstLine.positionCount = 2;
+        //secondLine.positionCount = 2;
 
-        // Vector2 secondStart = -9, 1.8
-        // Vector2 secondEnd = 15 , -3
+        //firstLine.SetPosition(0, startPoint);
+        //firstLine.SetPosition(1, endPoint);
 
-        firstLine.SetPosition(0, startPoint);
-        firstLine.SetPosition(1, endPoint);
-
-        secondLine.SetPosition(0, tempVectors.Item1);
-        secondLine.SetPosition(1, tempVectors.Item2);
+        //secondLine.SetPosition(0, tempVectors.Item1);
+        //secondLine.SetPosition(1, tempVectors.Item2);
 
         // lineRenderer.SetPosition(linePosObjects.Count, linePosObjects[0].position);
+    }
+    public void SetupLine(int lineIndex, Vector2 startPoint, Vector2 endPoint)
+    {
+        if(lineIndex == 0)
+        {
+            firstLine.positionCount = 2;
+            firstLine.SetPosition(0, startPoint);
+            firstLine.SetPosition(1, endPoint);
+        }
+        else if(lineIndex == 1)
+        {
+            secondLine.positionCount = 2;
+            secondLine.SetPosition(0, startPoint);
+            secondLine.SetPosition(1, endPoint);
+        }
+        
     }
 
 
     public void SetupPos(GameObject obj, Vector2 paramPos)
     {
         obj.transform.position = paramPos;
+    }
+
+
+    [TestMethod(false)]
+    public void ShowMidPoint(float firstX, float firstY,
+        float secondX, float secondY)
+    {
+
+        Vector2 startPoint = new Vector2(firstX, firstY);
+        Vector2 endPoint = new Vector2(secondX, secondY);
+        var firstMidPoint = MathHelperFunctions.GetMidPointOfLine(startPoint, endPoint);
+        // var tempVectors = MathHelperFunctions.GetPerpendicularLine(startPoint, endPoint, 10, 0, 0);
+        var tempVectors = MathHelperFunctions.GetPerpendicularLineAtMidPoint(startPoint, endPoint, firstMidPoint, 10);
+
+        SetupPos(firstLineStartPointObj, startPoint);
+        SetupPos(firstLineMidPointObj, firstMidPoint);
+
+        SetupPos(firstLineEndPointObj, endPoint);
+
+        var secondLineStartPoint = tempVectors.Item1;
+        var secondLineEndPoint = tempVectors.Item2;
+        var secondMidPoint = MathHelperFunctions.GetMidPointOfLine(secondLineStartPoint, secondLineEndPoint);
+        var tempSlope = MathHelperFunctions.GetSlopeOfLine(secondLineStartPoint, secondLineEndPoint);
+
+        SetupPos(secondLineStartPointObj, secondLineStartPoint);
+        SetupPos(secondLineMidPointObj, secondMidPoint);
+
+        SetupPos(secondLineEndPointObj, secondLineEndPoint);
+
+        // slope is -1/2
+        // y = mx + n
+        // 
+
+        var tempCosntant =  MathHelperFunctions.GetConstantOfLineEquation(firstMidPoint, tempSlope);
+        Debug.Log("temp slope is " + tempSlope
+            + " constant of line equation is " + tempCosntant);
+
+        SetupLine(0, startPoint, endPoint);
+        SetupLine(1, tempVectors.Item1, tempVectors.Item2);
+
     }
 
 }
--- Assets/LessonLearned/Maths/Scripts/MathHelperFunctions.cs
+++ Assets/LessonLearned/Maths/Scripts/MathHelperFunctions.cs
@@ -22,10 +22,11 @@ public class MathHelperFunctions : MonoBehaviour
         // line 2(this function's return value)
 
         // 1. get m(slope)
-        var modValueY = lineEndPoint.y - lineStartPoint.y;
-        var modValueX = lineEndPoint.x - lineStartPoint.x;
-        var firstLineSlope = modValueY / modValueX;
-        if(firstLineSlope == 0)
+        //var modValueY = lineEndPoint.y - lineStartPoint.y;
+        //var modValueX = lineEndPoint.x - lineStartPoint.x;
+        //var firstLineSlope = modValueY / modValueX;
+        var firstLineSlope = GetSlopeOfLine(lineStartPoint, lineEndPoint);
+        if (firstLineSlope == 0)
         {
             Debug.LogError("y / x is 0");
             // in this case, x = 10, y = 0 // x = 20 , y 0... 
@@ -65,5 +66,103 @@ public class MathHelperFunctions : MonoBehaviour
         }
         
 
+    }
+
+    public static (Vector2, Vector2) GetPerpendicularLineAtMidPoint(Vector2 lineStartPoint, Vector2 lineEndPoint,
+        Vector2 midPoint,
+        float lengthMod
+        )
+
+    {
+        // line 1 = y = mx+n
+        // line 2(this function's return value)
+
+        // 1. get m(slope)
+        //var modValueY = lineEndPoint.y - lineStartPoint.y;
+        //var modValueX = lineEndPoint.x - lineStartPoint.x;
+        //var firstLineSlope = modValueY / modValueX;
+
+        var firstLineSlope = GetSlopeOfLine(lineStartPoint, lineEndPoint);
+        if (firstLineSlope == 0)
+        {
+            Debug.LogError("y / x is 0");
+            // in this case, x = 10, y = 0 // x = 20 , y 0... 
+            // m == 0
+            // second line should be x 0, y = 10 // x = 0, y = 20
+            var secondLineFirstPointX = lineStartPoint.y;
+            var secondLineFirstPointY = lineStartPoint.x;
+
+            var secondLineSecondPointX = lineEndPoint.y;
+            var secondLineSecondPointY = lineEndPoint.x;
+
+            Vector2 secondLineFirstPoint = new Vector2(secondLineFirstPointX, secondLineFirstPointY);
+            Vector2 secondLineSecondPoint = new Vector2(secondLineSecondPointX, secondLineSecondPointY);
+            return (secondLineFirstPoint, secondLineSecondPoint);
+        }
+        else
+        {
+            var secondLineSlope = -(1 / firstLineSlope);
+            var currentConstant = GetConstantOfLineEquation(midPoint, secondLineSlope);
+
+            float positionModX = 0f; // it's x of line.. in future, maybe in use
+            float positionModY = currentConstant;
+
+
+            var secondLineFirstPointX = lineStartPoint.x - lengthMod + positionModX;
+            var secondLineFirstPointY = (secondLineFirstPointX * secondLineSlope) + positionModY;
+
+            var secondLineSecondPointX = lineEndPoint.x + lengthMod + positionModX;
+            var secondLineSecondPointY = (secondLineSecondPointX * secondLineSlope) + positionModY;
+
+            Vector2 secondLineFirstPoint = new Vector2(secondLineFirstPointX, secondLineFirstPointY);
+            Vector2 secondLineSecondPoint = new Vector2(secondLineSecondPointX, secondLineSecondPointY);
+
+            // 2. first line's slope * second line's slope  = -1
+            // m * m` = -1
+            //case 1. m = 5, point 1 (1,5) point 2(3,15)
+            // then m` = -1/5
+            // y = -0.2*x+n
+            // x = 1f, y = -0.2f
+            // x = 3f, y = -0.6f
+            return (secondLineFirstPoint, secondLineSecondPoint);
+        }
+
+
+    }
+
+
+    public static float GetSlopeOfLine(Vector2 lineStartPoint, Vector2 lineEndPoint)
+    {
+        float resultSlope = 0f;
+        // m = (y2 - y1) / (x2 - x1)
+        var modValueY = lineEndPoint.y - lineStartPoint.y;
+        var modValueX = lineEndPoint.x - lineStartPoint.x;
+        resultSlope = modValueY / modValueX;
+        return resultSlope;
+    }
+
+    public static float GetConstantOfLineEquation(Vector2 onePoint, float slope)
+    {
+        // ex 1)
+        // mid point is 1,2, slope is -1/2
+        // y = mx + n
+        // 2 = (-1/2 * 1) + n
+        // n = 5/2
+        float resultConstant = 0f;
+        // y - mx = n
+        var modX = onePoint.x;
+        var modY = onePoint.y;
+        resultConstant = modY - (slope * modX);
+        return resultConstant;
+
+    }
+
+    public static Vector2 GetMidPointOfLine(Vector2 lineStartPoint, Vector2 lineEndPoint)
+    {
+        Vector2 resultMidPoint = Vector2.zero;
+        var midPointX = (lineStartPoint.x + lineEndPoint.x) / 2f;
+        var midPointY = (lineStartPoint.y + lineEndPoint.y) / 2f;
+        resultMidPoint = new Vector2(midPointX, midPointY);
+        return resultMidPoint;
     }
 }
