a6e1be2815801da309c07386215589f61de4b316
Sandalwood <ebonytroll@gmail.com>
2023-09-25 18:36:29 +0900

    UI modding practices

--- Assets/Toy_1_Quaterview/Scripts/ModdingSetup/ModdingUIPracs.cs
+++ Assets/Toy_1_Quaterview/Scripts/ModdingSetup/ModdingUIPracs.cs
@@ -90,11 +90,16 @@ public class ModdingUIPracs : MonoBehaviour
     public Sprite fillSprite;
     public Texture fillMaskedGaugeSprite;
 
+    public Texture2D fillMaskedGaugeSpriteForTwoDimention;
+
     #endregion
 
     #region create UIs
     public void InitSliderUI()
     {
+        ProvideParamAndInit();
+        return;
+        
 
         var currentSample = sampleBackgroundImage;
 
@@ -182,10 +187,139 @@ public class ModdingUIPracs : MonoBehaviour
 
         var fillMaskedRawImageComp = fillMaskedGaugeObject.AddComponent<RawImage>();
         fillMaskedRawImageComp.texture = fillMaskedGaugeSprite;
+        // fillMaskedRawImageComp.texture = fillMaskedGaugeSpriteForTwoDimention;
 
         var fillMaskedrect = AddAndGetRectComp(fillMaskedGaugeObject);
         fillMaskedrect.anchoredPosition = Vector2.zero;
         fillMaskedrect.sizeDelta = new Vector2(10f,200f);
+        fillMaskedrect.anchorMin = new Vector2(0.5f, 0f);
+        fillMaskedrect.anchorMax = new Vector2(0.5f, 0f);
+        fillMaskedrect.pivot = new Vector2(0.5f, 0f);
+
+
+        // test
+        sliderComponent.value = 0.4f;
+
+    }
+
+    public void ProvideParamAndInit()
+    {
+        var textureFilePath = "D:\\Lab\\UnityModManaging\\ResourcesForUICreation\\Frame2.png";
+        // "D:\Lab\UnityModManaging\ResourcesForUICreation\Frame1.png"
+        Texture2D outTexture = null;
+        HelperFunctions.CreateTextureFromFile(textureFilePath, out outTexture);
+
+        var spriteFilePath = "D:\\Lab\\UnityModManaging\\ResourcesForUICreation\\Frame1.png";
+        Texture2D outTexture_2 = null;
+
+        Vector4 border = new Vector4(10f, 10f, 10f, 10f);
+
+        var outSprite = HelperFunctions.CreateSpriteFromFile(spriteFilePath, ref outTexture_2, border);
+
+        InitSliderUIFromOutside(parentCanvas, outSprite, outTexture);
+
+    }
+
+    public void InitSliderUIFromOutside(
+            // RectTransform sampleBackgroundImage,
+            Canvas parentCanvas,
+            Sprite backGroundSprite,
+            Texture2D fillMaskedGaugeSprite
+            )
+    {
+        // write down UI for fill bar gauge
+
+
+        // var currentSample = sampleBackgroundImage;
+
+        // Vector2 parentRect = new Vector2(255f, 0.5f);
+        // Vector2 sliderRectPos = new Vector2(255f, 0.5f);
+
+        var currentObject = new GameObject();
+        currentObject.name = "createdSliderParent";
+        currentObject.transform.parent = parentCanvas.transform;
+        var parentRectTrans = currentObject.AddComponent<RectTransform>();
+        if (parentRectTrans != null)
+        {
+            parentRectTrans.anchoredPosition = new Vector2(-12f, 0f);
+        }
+        // parentRectTrans.anchoredPosition = parentRect;
+
+        parentRectTrans.anchorMin = new Vector2(1f, 0.5f);
+        parentRectTrans.anchorMax = new Vector2(1f, 0.5f);
+
+
+
+        var sliderObject = new GameObject();
+        sliderObject.transform.parent = currentObject.transform;
+        var sliderComponent = sliderObject.AddComponent<Slider>();
+        sliderComponent.transition = Selectable.Transition.None;
+        sliderComponent.direction = Slider.Direction.BottomToTop;
+        sliderComponent.minValue = 0f;
+        sliderComponent.maxValue = 1f;
+
+
+        var currentSliderRect = sliderObject.GetComponent<RectTransform>();
+        // currentSliderRect.anchoredPosition = sliderRectPos;
+        currentSliderRect.anchoredPosition = Vector2.zero;
+        currentSliderRect.sizeDelta = new Vector2(20f, 160f);
+        // currentSliderRect. = new Rect(0f, 0f, 20f, 180f);
+        sliderObject.name = "Slider";
+
+        // background
+        var backgroundObject = new GameObject();
+        backgroundObject.transform.parent = sliderObject.transform;
+        // backgroundObject.name = "Background";
+        backgroundObject.name = "Fill Area";
+
+        // var backgroundRect = backgroundObject.GetComponent<RectTransform>();
+        var backgroundRect = backgroundObject.AddComponent<RectTransform>();
+        backgroundRect.anchorMin = new Vector2(0.25f, 0f);
+        backgroundRect.anchorMax = new Vector2(0.75f, 1f);
+        backgroundRect.pivot = new Vector2(0.5f, 0.5f);
+        // WaitAndCall(0.1f, () => backgroundRect.sizeDelta = new Vector2(10f, 160f));
+        backgroundRect.anchoredPosition = Vector2.zero;
+        // backgroundRect.sizeDelta = new Vector2(10f, 160f);
+        backgroundRect.offsetMin = Vector2.zero;
+        backgroundRect.offsetMax = Vector2.zero;
+
+
+        var backgroundImage = backgroundObject.AddComponent<Image>();
+        backgroundImage.sprite = backGroundSprite;
+        backgroundImage.type = Image.Type.Sliced;
+
+        var backgroundMask = backgroundObject.AddComponent<Mask>();
+
+
+        // need to change background -> fill area
+
+        var fillObject = new GameObject();
+        fillObject.transform.parent = backgroundObject.transform;
+        fillObject.name = "Fill";
+        var fillImage = fillObject.AddComponent<Image>();
+        fillImage.sprite = null;
+        // fillImage.type = Image.Type.Sliced;
+
+        var fillObjectMaskComp = fillObject.AddComponent<Mask>();
+        // var fillRect = fillObject.AddComponent<RectTransform>();
+        var fillRect = AddAndGetRectComp(fillObject);
+        sliderComponent.fillRect = fillRect;
+        fillRect.anchoredPosition = Vector2.zero;
+        fillRect.offsetMin = Vector2.zero;
+        fillRect.offsetMax = Vector2.zero;
+        fillRect.pivot = new Vector2(0.5f, 0f);
+
+
+        var fillMaskedGaugeObject = new GameObject();
+        fillMaskedGaugeObject.transform.parent = fillObject.transform;
+        fillMaskedGaugeObject.name = "FillMaskedGauge";
+
+        var fillMaskedRawImageComp = fillMaskedGaugeObject.AddComponent<RawImage>();
+        fillMaskedRawImageComp.texture = fillMaskedGaugeSprite;
+
+        var fillMaskedrect = AddAndGetRectComp(fillMaskedGaugeObject);
+        fillMaskedrect.anchoredPosition = Vector2.zero;
+        fillMaskedrect.sizeDelta = new Vector2(10f, 200f);
         fillMaskedrect.anchorMin = new Vector2(0.5f, 0f);
         fillMaskedrect.anchorMax = new Vector2(0.5f, 0f);
         fillMaskedrect.pivot = new Vector2(0.5f, 0f);
