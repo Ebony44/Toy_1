920efdc699c90158791c82b8f6faf02398523f24
DESKTOP-BLIU7UA\user <ebonytroll@gmail.com>
2023-12-05 21:05:11 +0900

    Scene and script for dragging modified

--- Assets/Toy_1_Quaterview/Scripts/UIControl/Draggable.cs
+++ Assets/Toy_1_Quaterview/Scripts/UIControl/Draggable.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    public GameObject tempTestObj;
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("[OnBeginDrag]");
@@ -14,6 +23,19 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDra
     {
         
         this.transform.position = eventData.position;
+        Vector2 mousepos = new Vector2();
+        Vector3 point = new Vector3();
+        // mousepos.x = currentEvent.mousePosition.x;
+        // mousepos.y = currentEvent.mousePosition.y;
+
+        
+
+        mousepos.x = Mouse.current.position.x.ReadValue();
+        mousepos.y = Mouse.current.position.y.ReadValue();
+        point = cam.ScreenToWorldPoint(new Vector3(mousepos.x, mousepos.y, cam.nearClipPlane));
+        Debug.Log("mouse pos is " + mousepos
+            + " point is " + point);
+        tempTestObj.transform.position = point;
     }
 
     public void OnEndDrag(PointerEventData eventData)
