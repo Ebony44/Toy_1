3f48c8fa6b43d3c8f9d3af041573fb143f10d083
Sandalwood <ebonytroll@gmail.com>
2023-12-19 17:51:43 +0900

    Item creating WIP

--- Assets/Toy_1_Quaterview/Scripts/ItemCreating/ItemCreateManager.cs
+++ Assets/Toy_1_Quaterview/Scripts/ItemCreating/ItemCreateManager.cs
@@ -4,6 +4,41 @@ using UnityEngine;
 
 public class ItemCreateManager : MonoBehaviour
 {
+
+    Dictionary<EItemType, ItemGeneralTypeInfo> itemInfos = new Dictionary<EItemType, ItemGeneralTypeInfo>();
+
+    public void RollItem()
+    {
+
+    }
+
+    public void InitializeWeights()
+    {
+        // TODO: 
+        // psuedo code
+        // if weight 100 options are 2,
+        // 0~99 -> first option
+        // 100~199 ->  second option
+        var tempRandom = Random.Range(0, 100);
+
+        // test
+
+        ItemWeightInfo lifeTier3 = new ItemWeightInfo();
+        lifeTier3.currentType = EItemOptionType.MaxLife;
+        lifeTier3.weight = 200;
+
+        ItemWeightInfo lifeTier2 = new ItemWeightInfo();
+        lifeTier2.currentType = EItemOptionType.MaxLife;
+        lifeTier2.weight = 100;
+
+
+    }
+
+    public void SortOutWeight(List<ItemWeightInfo> paramList)
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +50,34 @@ public class ItemCreateManager : MonoBehaviour
     {
         
     }
+}
+
+public enum EItemType
+{
+    None = 0,
+    BionicJewel,
+}
+public enum EItemOptionType
+{
+    None = 0,
+    MaxLife,
+}
+public class ItemGeneralTypeInfo
+{
+    public EItemType currentType;
+    public int totalWeight;
+    public List<ItemWeightInfo> weightInfos = new List<ItemWeightInfo>(64);
+
+    // public List<>
+}
+
+public class ItemWeightInfo
+{
+    public EItemOptionType currentType;
+    public int weight;
+
+    // modify AFTER instantiate
+    public int currentWeightRangeMin;
+    public int currentWeightRangeMax;
+
 }
