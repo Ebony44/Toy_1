e2524e521be5529ebc582dc685274d78a0cceacc
DESKTOP-BLIU7UA\user <ebonytroll@gmail.com>
2024-04-20 21:52:52 +0900

    Simple labs

---
+++ Assets/Tutorials/Various/GridInventorySystem/Scripts/ItemGrid.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrid : MonoBehaviour
+{
+    Vector2Int GetGridPosition(Vector2 mousePos)
+    {
+        Vector2Int result = null;
+
+        return result;
+    }
+}
