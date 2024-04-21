83c77ab5e19ce590bfb45a5d8da3b19a2ffc24aa
DESKTOP-BLIU7UA\user <ebonytroll@gmail.com>
2024-04-21 13:14:11 +0900

    previously added sprites meta changed

--- Assets/Tutorials/Various/GridInventorySystem/Scripts/ItemGrid.cs
+++ Assets/Tutorials/Various/GridInventorySystem/Scripts/ItemGrid.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemGrid : MonoBehaviour
+namespace InventoryLab
 {
-    Vector2Int GetGridPosition(Vector2 mousePos)
+    public class ItemGrid : MonoBehaviour
     {
-        Vector2Int result = null;
 
-        return result;
+        const float tileSizeWidth = 32;
+        const float tileSizeHeight = 32;
+        RectTransform rectTransform;
+
+        Vector2 positionOnTheGrid = new Vector2();
+        Vector2Int tileGridPosition = new Vector2Int();
+
+        private void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+
+        }
+
+
+        public Vector2Int GetTileGridPosition(Vector2 mousePos)
+        {
+            // Vector2Int result = new Vector2Int();
+            positionOnTheGrid.x = mousePos.x - rectTransform.position.x;
+            positionOnTheGrid.y = mousePos.y - rectTransform.position.y;
+
+            tileGridPosition.x = (int)(positionOnTheGrid.x / tileSizeWidth);
+            tileGridPosition.y = (int)(positionOnTheGrid.y / tileSizeHeight);
+
+            return tileGridPosition;
+        }
     }
+
 }
+
