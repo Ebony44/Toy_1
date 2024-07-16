0584767ef6aa83a91dc02250451552a8290896c0
Sandalwood <ebonytroll@gmail.com>
2024-07-16 17:55:05 +0900

    URP Decal projector testing

---
+++ Assets/Toy_1_Quaterview/Scripts/TestScripts/DecalTestingManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DecalTestingManager : MonoBehaviour
+{
+    DecalProjector targetDecal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void ShowDecal()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
