4597e575760ff60623ee984116f72f6250959486
Sandalwood <ebonytroll@gmail.com>
2025-04-23 14:30:14 +0900

    Examines concepts

--- Assets/PlayGrounds/RobotArena/Scripts/VelocityChecker.cs
+++ Assets/PlayGrounds/RobotArena/Scripts/VelocityChecker.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class VelocityChecker : MonoBehaviour
 {
-    SphereCollider sphereCollider;
-    Rigidbody rb;
+    [SerializeField] private SphereCollider sphereCollider;
+    [SerializeField] private Rigidbody rb;
+
+    private bool bIsOnCollisionStayCalled = false;
+
+    private void Start()
+    {
+        Invoke("LogVelocity", 0.5f);
+    }
+
     [TestMethod(false)]
     public void LogVelocity()
     {
@@ -14,18 +22,50 @@ public class VelocityChecker : MonoBehaviour
             rb = GetComponent<Rigidbody>();
         }
         
-        Debug.Log("Current Velocity: " + rb.linearVelocity);
+        Debug.Log("Current Velocity: " + rb.linearVelocity
+            + " object name is " + gameObject.name);
 
     }
 
-    public void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("OnTriggerEnter: " + other.name);
+        Debug.Log("OnCollisionEnter: " + collision.gameObject.name
+            + " relative velocity is " + collision.relativeVelocity
+            + " this obj name " + gameObject.name);
         LogVelocity();
+
     }
-    public void OnTriggerExit(Collider other)
+    private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("OnTriggerExit: " + other.name);
+        Debug.Log("OnCollisionExit: " + collision.gameObject.name);
+        bIsOnCollisionStayCalled = false;
     }
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if (bIsOnCollisionStayCalled)
+    //    {
+    //        return;
+    //    }
+    //    if (rb == null)
+    //    {
+    //        rb = GetComponent<Rigidbody>();
+    //    }
+        
+
+    //    bIsOnCollisionStayCalled = true;
+    //    Debug.Log("OnCollisionStay: " + collision.gameObject.name);
+    //    Debug.Log("Velocity before collision: " + rb.linearVelocity);
+        
+    //}
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("OnTriggerEnter: " + other.name);
+    //    LogVelocity();
+    //}
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("OnTriggerExit: " + other.name);
+    //}
 
 }
