c629a45d76aa50a281a36c01464d02c15e30a21a
Sandalwood <ebonytroll@gmail.com>
2025-06-23 17:20:19 +0900

    Fixing some parameter error in equation

--- Assets/Toy_1_Quaterview/Scripts/TestScripts/TrajectoryTester.cs
+++ Assets/Toy_1_Quaterview/Scripts/TestScripts/TrajectoryTester.cs
@@ -2,6 +2,7 @@
 // testing trajectory simulation
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class TrajectoryTester : MonoBehaviour
@@ -14,9 +15,13 @@ public class TrajectoryTester : MonoBehaviour
     public GameObject explosion;
 
     public Transform shooterTrans;
+    // public PlayerInput playerInput;
+    private GameInput gameInput;
 
-    SerializeField] float speed = 15f;
-    [SerializeField] float rotSpeed = 2f;
+    [SerializeField] float speed = 25f;
+    [SerializeField] float rotSpeed = 8f;
+
+    [SerializeField] bool bIsUpdateLogicEnabled = true;
 
     //[SerializeField] float speed = 0f;
     //[SerializeField] float ySpeed = 0f;
@@ -46,15 +51,7 @@ public class TrajectoryTester : MonoBehaviour
         }
     }
 
-    public void LookOnTo()
-    {
-        Vector3 direction = target.transform.position - this.transform.position.normalized;
-        // Quaternion lookRot = Quaternion.LookRotation(direction);
-        Quaternion lookRot = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRot, Time.deltaTime * rotSpeed);
-
-    }
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -63,47 +60,112 @@ public class TrajectoryTester : MonoBehaviour
         {
             shooterTrans = shooter.transform;
         }
+
+        
+        
+        
+    }
+    private void OnEnable()
+    {
+        if(gameInput == null)
+        {
+            gameInput = new GameInput();
+        }
+        gameInput.Enable();
+        gameInput.Player.Space.performed += OnPressSpace;
+        // gameInput.Player.Space.started += OnPressSpace;
+        // difference between performed and started is that performed is called when the button is pressed down,
+        // while started is called when the button is pressed down and held.
+        
+    }
+    private void OnDisable()
+    {
+        if(gameInput != null)
+        {
+            gameInput.Player.Space.performed -= OnPressSpace;
+            // gameInput.Player.Space.started -= OnPressSpace;
+            gameInput.Disable();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!bIsUpdateLogicEnabled)
+        {
+            return;
+        }
+        LookOnTo();
+        Rotate();
+        CalculateAngle(true); // Calculate the angle for low trajectory
     }
-
-    public void TestTrajectory()
+    public void OnPressSpace(InputAction.CallbackContext callbackContext)
     {
-        // vx = v0 * cos(��)
-        // vy = v0 * sin(��) - (g * t)
-
-        // speed = root of (vx^2 + vy^2)
-
-        
+        // bool isPressed = callbackContext.performed || callbackContext.started;
+        float pressValue = callbackContext.ReadValue<float>();
+        //float currentAxis = callbackContext.ReadValue<float>();
+        Debug.Log("Space Pressed: " + pressValue);
+        //scrollInput.y = currentAxis > 0 ? 1
+        //    : currentAxis < 0 ? -1
+        //    : 0;
+        FireBullet();
     }
-    public void FireBullet()
+
+    public void LookOnTo()
     {
-        GameObject currentBullet = Instantiate(bulletPrefab, shooterTrans.position, shooterTrans.rotation);
-        currentBullet.GetComponent<Rigidbody>().angularVelocity =  speed * shooterTrans.forward; // Set the bullet's velocity in the direction the shooter is facing
+        Vector3 direction = (target.transform.position - shooter.transform.position).normalized;
+        // Quaternion lookRot = Quaternion.LookRotation(direction);
+        Quaternion lookRot = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        shooter.transform.rotation = Quaternion.Slerp(shooter.transform.rotation, lookRot, Time.deltaTime * rotSpeed);
+        // float? angle = Rotate();
+
 
     }
+    float? Rotate()
+    {
+        float? angle = CalculateAngle(true);
+        Debug.Log($"Calculated angle: {angle} degrees");
+        if (angle.HasValue)
+        {
+            shooterTrans.localEulerAngles = new Vector3(360 - (float)angle, 0, 0); // Adjust the angle to match Unity's coordinate system
+            
+            //shooterTrans.localEulerAngles = new Vector3(360 - (float)angle,
+            //    shooterTrans.transform.localEulerAngles.y,
+            //    shooterTrans.transform.localEulerAngles.z); // Adjust the angle to match Unity's coordinate system
+
+            //shooterTrans.localEulerAngles = new Vector3(0, 360 - (float)angle, 0); // Adjust the angle to match Unity's coordinate system
+            //Quaternion targetRotation = Quaternion.Euler(0, angle.Value, 0);
+            //shooter.transform.rotation = Quaternion.Slerp(shooter.transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
+        }
+        else
+        {
+            Debug.LogError("Failed to calculate angle for rotation.");
+        }
+        return angle;
+    }
+
 
     private float? CalculateAngle(bool low)
     {
         float result = 0f;
-        Vector3 targetDir = target.transform.position - this.transform.position;
+        Vector3 targetDir = target.transform.position - shooter.transform.position;
         float y = targetDir.y;
         targetDir.y = 0f; // Ignore the y component for horizontal angle calculation
         // float x = targetDir.magnitude; // dot magitude
-        float x = targetDir.magnitude - 1;
+        float x = targetDir.magnitude - 1.0f;
         float gravity = -Physics.gravity.y; // Assuming gravity is negative in Unity
-        Debug.Log($"Target Direction: {targetDir}, Y: {y}, X: {x}, Gravity: {gravity}");
+        // Debug.Log($"Target Direction: {targetDir}, Y: {y}, X: {x}, Gravity: {gravity}");
         float sSqr = speed * speed;
         // result will be 2 possible angles, one for low and one for high trajectory
         // Tan(a) = sSqr + root of (sSqr^2 - g * (g * x^2 + 2 * y * sSqr)) / (g * x)
         // Tan(a) = sSqr - root of (sSqr^2 - g * (g * x^2 + 2 * y * sSqr)) / (g * x)
-        float underTheSqrRoot = sSqr * sSqr - gravity * ( (gravity * x * x) + (2f * y * sSqr) );
 
-        if(underTheSqrRoot < 0f)
+        // float underTheSqrRoot = sSqr * sSqr - gravity * ( (gravity * x * x) + (2f * y * sSqr) );
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+        Debug.Log("magnitude: " + targetDir.magnitude + ", x: " + x + ", y: " + y + ", gravity: " 
+            + gravity + ", sSqr: " + sSqr + ", underTheSqrRoot: " + underTheSqrRoot);
+        if (underTheSqrRoot < 0f)
         {
             Debug.LogError("No valid angle found for the given parameters. Under the square root is negative.");
             return null;
@@ -125,23 +187,92 @@ public class TrajectoryTester : MonoBehaviour
             // float tanAngle = (sSqr + Mathf.Sqrt(underTheSqrRoot)) / (gravity * x);
         }
 
+        // return result;
+        return null;
+    }
+
+    public void FireBullet()
+    {
+        GameObject currentBullet = Instantiate(bulletPrefab, shooterTrans.position, shooterTrans.rotation);
+        currentBullet.GetComponent<Rigidbody>().linearVelocity =  speed * shooterTrans.forward; // Set the bullet's velocity in the direction the shooter is facing
+
+    }
+    
+
+
+    private void CalculateBulletMidPointAngle(Vector3 start, Vector3 end)
+    {
+        // generate mid point between start and end
+        // with slightly positive of y value?
+        Vector3 midPoint = (start + end) / 2f;
+        midPoint.y += 1f; // Adjust the y value to be slightly above the midpoint
+
+        //then vector lerp with mid point?
+        float currentTime = 0f;
+        var startVector = Vector3.Lerp(start, midPoint, currentTime);
+        var endVector = Vector3.Lerp(midPoint, end, currentTime);
+        var result = Vector3.Lerp(startVector, endVector, currentTime);
+        
+        
+
+        
+
+    }
+
+    // https://foo897.tistory.com/24
+    #region rip from 
+    void LaucherProjecttile()
+    {
+        Vector3 Vo = CalculateVelcoity(target.transform.position, transform.position, 1f);
+        // Rigidbody obj = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+        Rigidbody obj = Instantiate(bulletPrefab, target.transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+        obj.linearVelocity = Vo;
+        //setTrajectoryPoints(transform.position, Vo);
+        DrawPath(Vo);
+    }
+
+    //�� ����� ��ǥ ���Ϳ� ������ �������� �ʿ��մϴ�.
+    //time : ����ð�
+    Vector3 CalculateVelcoity(Vector3 target, Vector3 origin, float time)
+    {
+        //define the distance x and y first
+        Vector3 distance = target - origin;
+        Vector3 distanceXZ = distance; //x��z�� ����̸� �⺻������ �Ÿ��� ���� ����
+        distanceXZ.y = 0f;//y�� 0���� ����
+
+        //create a float the represent our distance
+        float Sy = distance.y;//���� ������ �Ÿ��� ����
+        float Sxz = distanceXZ.magnitude;
+
+        //�ӵ� ���
+        float Vxz = Sxz / time;
+        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+        //������� ���� ������ �ʱ� �ӵ� ������ ���ο� ���͸� ����� ����
+        Vector3 result = distanceXZ.normalized;
+        result *= Vxz;
+        result.y = Vy;
         return result;
     }
 
-    void Rotate()
+    void DrawPath(Vector3 velocity)
     {
-        float? angle = CalculateAngle(true);
-        if(angle.HasValue)
+        Vector3 previousDrawPoint = transform.position;
+        int resolution = 30;
+        //lineRenderer.positionCount = resolution;
+        for (int i = 1; i <= resolution; i++)
         {
-            shooterTrans.localEulerAngles = new Vector3(360 - (float)angle.Value, 0, 0); // Adjust the angle to match Unity's coordinate system
-            //Quaternion targetRotation = Quaternion.Euler(0, angle.Value, 0);
-            //shooter.transform.rotation = Quaternion.Slerp(shooter.transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
-        }
-        else
-        {
-            Debug.LogError("Failed to calculate angle for rotation.")
+            //float simulationTime = i / (float)resolution * launchData.timeToTarget;
+            float simulationTime = i / (float)resolution * 1f;
+
+            Vector3 displacement = velocity * simulationTime + Vector3.up * Physics.gravity.y * simulationTime * simulationTime / 2f;
+            Vector3 drawPoint = transform.position + displacement;
+            // DebugExtension.DebugPoint(drawPoint, 1, 1000f);//����Ƽ ���½���� Debug Extension
+            Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+            //lineRenderer.SetPosition(i - 1, drawPoint);
+            previousDrawPoint = drawPoint;
         }
     }
-
+    #endregion
 
 }
